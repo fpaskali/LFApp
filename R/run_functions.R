a2514f0ecdc065ae364af70635c43c977c9c9c4a
stamats <Matthias.Kohl@stamats.de>
2021-04-17 20:23:51 +0200

    added run functions for mobile verisons

--- R/run_functions.R
+++ R/run_functions.R
@@ -1,3 +1,4 @@
+## desktop versions
 run_analysis <- function(){
   shinyApp(ui = analysis_ui, server = analysis_server)
 }
@@ -9,4 +10,18 @@ run_core <- function(){
 }
 run_quan <- function(){
   shinyApp(ui = quan_ui, server = quan_server)
+}
+
+## mobile versions
+run_mobile_analysis <- function(){
+  shinyApp(ui = analysis_mobile_ui, server = analysis_mobile_server)
+}
+run_mobile_cal <- function(){
+  shinyApp(ui = cal_mobile_ui, server = cal_mobile_server)
+}
+run_mobile_core <- function(){
+  shinyApp(ui = core_mobile_ui, server = core_mobile_server)
+}
+run_mobile_quan <- function(){
+  shinyApp(ui = quan_mobile_ui, server = quan_mobile_server)
 }

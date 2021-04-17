710e21714903f9bd3e4ef3a61abb8792c361d547
stamats <Matthias.Kohl@stamats.de>
2021-04-17 14:57:24 +0200

    re-implementation of triangle

--- R/triangle.R
+++ R/triangle.R
@@ -1,37 +1,47 @@
-#' Triangle algorithm
-#'
-#' @export
-
 triangle <- function(image, offset = 0.2, breaks = 256) {
-  image_hist <- hist(image, breaks = breaks, plot = FALSE)
-  hist_counts <- image_hist$counts
-  hist_breaks <- image_hist$breaks
-
-  # Finds the locaton of peak, the peak value.
-  arg_peak_height <- which.max(hist_counts)
-  peak_height <- hist_counts[arg_peak_height]
-
-  # Flip is true if the right tail is shorter.
-  flip <- arg_peak_height > breaks / 2
-  if (flip == TRUE) {
-    hist_counts <- rev(hist_counts)
-    arg_peak_height <- which.max(hist_counts)
+  if(!is(image, "Image"))
+    stop("'image' must be of class 'Image'!")
+  breaks <- as.integer(breaks)
+  stopifnot(offset >= 0)
+  stopifnot(breaks > 0)
+  
+  ## compute histogram and extract counts and breaks
+  rg <- range(image)
+  bins <- breaks
+  breaks <- seq(rg[1], rg[2], length = breaks + 1L)
+  image.hist <- hist.default(imageData(image), breaks = breaks, plot = FALSE)
+  hist.counts <- image.hist$counts
+  hist.breaks <- image.hist$breaks
+  
+  ## centers of bins
+  delta <- hist.breaks[2]-hist.breaks[1]
+  hist.bins <- hist.breaks[-(bins+1)] + delta/2
+  
+  ## location of peaks and peak value
+  ind.peaks <- which(hist.counts == max(hist.counts))
+  ind.first.peak <- ind.peaks[1]
+  ind.last.peak <- ind.peaks[length(ind.peaks)]
+  peak.height <- hist.counts[ind.first.peak]
+  
+  ## fist and last bin with positive count
+  pos.counts <- which(hist.counts > 0)
+  ind.low <- pos.counts[1]
+  ind.high <- pos.counts[length(pos.counts)]
+  
+  if((ind.first.peak - ind.low) < (ind.high - ind.last.peak)){
+    ## right tail is longer
+    sel <- ind.last.peak:ind.high
+    norm.counts <- hist.counts[sel]/peak.height
+    norm.bins <- (1:length(sel))/length(sel)
+    distances <- (1-norm.counts)*(1-norm.bins)/sqrt((1-norm.counts)^2 + (1-norm.bins)^2)
+  }else{
+    ## left tail is longer
+    sel <- ind.low:ind.first.peak
+    norm.counts <- hist.counts[sel]/peak.height
+    norm.bins <- (1:length(sel))/length(sel)
+    distances <- (1-norm.counts)*norm.bins/sqrt((1-norm.counts)^2 + norm.bins^2)
   }
-
-  # Width is the length from the position of the highest peak, to the end.
-  width <- breaks - arg_peak_height
-  heights <- hist_counts[arg_peak_height:breaks]
-  # Scaling the heights to be equal to the width.
-  normalizer <- peak_height / width
-  heights <- heights / normalizer
-
-  # The distance from highest peak and the each height. Used to measure the distance.
-  triangle_side <- width - heights
-  distances <- sqrt(2*triangle_side**2)/2
-  # Finding the position of the maximal distance.
-  arg_max_distance <- which.max(distances)
-  # Adding the fixed offset to the maximal distance position.
-  arg_threshold <- arg_peak_height + arg_max_distance + width*offset
-
-  return(hist_breaks[arg_threshold])
+  ind.max <- which.max(distances)
+  
+  hist.bins[sel[ind.max]] + offset
 }

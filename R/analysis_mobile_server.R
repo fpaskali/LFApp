37d19819dea15a6ee80674dca1cef06b44f31ca8
stamats <Matthias.Kohl@stamats.de>
2021-04-24 14:25:41 +0200

    adapted computation of LOB, LOD, LOQ and fitted values

--- R/analysis_mobile_server.R
+++ R/analysis_mobile_server.R
@@ -17,6 +17,7 @@ analysis_mobile_server <- function(input, output, session){
   calFun <- NULL
   quanData <- NULL
   CalibrationData <- NULL
+  predFunc <- NULL
   predictData <- NULL
   
   # checks upload for file imput
@@ -934,14 +935,10 @@ analysis_mobile_server <- function(input, output, session){
       # Adding the analysis name and model formula to the table
       modelName <- rep(modelName, nrow(CalibrationData))
       modelFormula <- rep(FORMULA, nrow(CalibrationData))
-      if (input$chosenModel == "Local polynomial model (loess)") {
-        modelDF <- cbind(modelName, modelFormula, fit$fitted)
-      } else {
-        modelDF <- cbind(modelName, modelFormula, fit$fitted.values)
-      }
+      modelDF <- cbind(modelName, modelFormula, predFunc(CalibrationData))
       colnames(modelDF) <- c(paste0(input$analysisName, ".model"), 
                              paste0(input$analysisName, ".formula"), 
-                             paste0(input$analysisName, ".fit"))
+                             paste0(input$analysisName, ".", input$concVar, ".fit"))
       DF <- cbind(CalibrationData, modelDF)
       CalibrationData <<- DF
       
